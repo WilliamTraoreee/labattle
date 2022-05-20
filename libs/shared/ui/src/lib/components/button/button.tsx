6a670f68348ad1b5838cd7a57f244b98eb1f72c5
Wiliam Traoré <hi@wil-liam.com>
2022-05-20 18:09:29 +0200

    feat(project): add tailwind + storybook

---
+++ libs/shared/ui/src/lib/components/button/button.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ButtonProps {}
+
+export function Button(props: ButtonProps) {
+  return (
+    <div>
+      <h1 className="text-center text-yellow-500">Welcome to Button!</h1>
+    </div>
+  );
+}
+
+export default Button;
